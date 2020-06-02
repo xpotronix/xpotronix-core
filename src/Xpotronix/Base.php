3de5d664313dc24e09f30ec91a0fed953d30368f
Eduardo Spotorno <espotorno@jusbaires.gov.ar>
2020-06-02 00:27:07 -0300

    orden batik

--- src/Xpotronix/Base.php
+++ src/Xpotronix/Base.php
@@ -290,8 +290,8 @@ class Base {
 			$jars = array( 
 				"/usr/share/java/commons-io.jar",
 				"/usr/share/java/avalon-framework.jar",
-				"/usr/share/java/batik-all.jar",
 				"/usr/share/java/xmlgraphics-commons.jar",
+				"/usr/share/java/batik-all.jar",
 				"/usr/share/java/serializer.jar",
 				"/usr/share/java/xalan2.jar",
 				"/usr/share/java/xml-apis.jar",
