db80e85ac9a95494dd9d4a21ae147b6b6df32468
xpotronix <xpotronix@gmail.com>
2022-08-04 13:42:47 -0300

    actualizacion install/

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+
+# requerimientos
+# apache2
+# php7.4-fpm o posterior
+# mysql8 una base de datos que sea xpay_mpf
+
+# disposicion de achivos en el sistema
+
 XPATH=/usr/share/xpotronix
 EXT_VERSION="3.4.0"
 PHP_VERSION="7.4"
 UG_PERMS=www-data.www-data
 UG_MODE=775
+CONFIG_PATH=/etc/xpotronix
+INIT_PATH=/etc/init.d
+APP_DOMAIN=jusbaires.gob.ar
 
 #http_proxy=http://username:password@host:port/
 #export http_proxy
@@ -31,8 +42,8 @@ fi
 mkdir -p $XPATH
 
 # directorios y permisos
-mkdir -p /etc/xpotronix
-chown $UG_PERMS /etc/xpotronix
+mkdir -p $CONFIG_PATH
+chown $UG_PERMS $CONFIG_PATH
 
 mkdir -p $DOCUMENT_ROOT
 chown $UG_PERMS  $DOCUMENT_ROOT
@@ -42,12 +53,40 @@ chown $UG_PERMS $DOCUMENT_ROOT/tmp
 
 cp -Rp ../* $XPATH
 
+# configuracion apache2
+
 echo $DOCUMENT_ROOT
-sed -s "s#DOCUMENT_ROOT#$DOCUMENT_ROOT#g" xpotronix.ini >/etc/xpotronix/xpotronix.ini
-sed -s "s#DOCUMENT_ROOT#$DOCUMENT_ROOT#g" xpotronix.conf >/etc/apache2/sites-enabled/xpotronix.conf
+sed -s "s#DOCUMENT_ROOT#$DOCUMENT_ROOT#g" xpotronix.ini >$CONFIG_PATH/xpotronix.ini
+sed -s "s#DOCUMENT_ROOT#$DOCUMENT_ROOT#g" $XPATH/install/apache2/xpotronix.conf | \
+sed -s "s#XPATH#$XPATH#g" $XPATH/install/apache2/xpotronix.conf | \
+sed -s "s#APP_DOMAIN#$APP_DOMAIN#g" $XPATH/install/apache2/xpotronix.conf > /etc/apache2/sites-enabled/xpotronix.conf
+
+>/etc/apache2/sites-enabled/xpotronix.conf
+
+
+# utilidades del bash para la transformacion
 
 ln -sf $XPATH/util/xpotronix /usr/bin/xpotronix
 ln -sf $XPATH/util/xpotronize /usr/bin/xpotronize
 ln -sf $XPATH/util/xpdumpbase-mysql /usr/bin/xpdumpbase-mysql
 ln -sf $XPATH/util/xputil /usr/bin/xputil
+
+
+# javscript y maquina virtual 
+
+# javascript
+
+wget -c http://justamente.net/downloads/ext-3.4.0.zip -O $XPATH/lib
+wget -c http://justamente.net/downloads/ext-4.2.2-gpl.zip -O $XPATH/lib
+
+
+# wrapper java
+wget -c http://justamente.net/downloads/JavaBridgeTemplate.war $XPATH/lib
+
+
+# la entrada init.d para el servicio de php-java-bridge
+
+sudo cp $XPATH/php-java-bridge/php-java-bridge $INIT_PATH/
+systemctl enable --now php-java-bridge
+
 
